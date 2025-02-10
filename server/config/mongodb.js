61c59acaa3432cb3a03ad53358e261a4dc236097
BasheerMohamed <Basheer_Mohamed@proton.me>
2025-02-10 21:54:23 +0530

    commit

--- server/config/mongodb.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-// Function to establish a connection to the MongoDB database...
-const connectDB = async () => {
-    mongoose.connection.on('connected', () => console.log('Database Connected...'));
-    await mongoose.connect(`${process.env.MONGODB_URI}/mern-auth`);
-};
-
-export default connectDB;
--- server/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import userAuth from '../middleware/userAuth.js';
-import { getUserData } from '../controllers/userController.js';
-
-const userRoutes = express.Router();
-
-userRoutes.get('/data', userAuth, getUserData);
-
-export default userRoutes;
